ffb0bf062fb0b22250b8aaa01fbb56e7021feb7a
Nathami-n <georgenathan010@gmail.com>
2025-03-13 01:17:32 +0300

    add: final commit for the day

--- lib/features/authentication/screens/home/home.dart
+++ lib/features/authentication/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/common/widgets/common_shapes/containers/circular_container.dart';
 import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -13,48 +14,31 @@ class HomeScreen extends StatelessWidget {
             Container(
               color: TColors.primary,
               padding: const EdgeInsets.all(0),
-              child: Stack(
-                children: [
-                  CircularContainerBackground(),
-                ],
+              child: SizedBox(
+                height: 400,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: -150,
+                      right: -250,
+                      child: CircularContainerBackground(
+                        backgroundColor: TColors.textWhite.withOpacity(0.1),
+                      ),
+                    ),
+                    Positioned(
+                      top: 100,
+                      right: -300,
+                      child: CircularContainerBackground(
+                        backgroundColor: TColors.textWhite.withOpacity(0.1),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
         ),
       ),
-    );
-  }
-}
-
-class CircularContainerBackground extends StatelessWidget {
-  const CircularContainerBackground({
-    super.key,
-    this.width = 400,
-    this.height = 400,
-    required this.radius,
-    required this.padding,
-    this.child,
-    required this.backgroundColor,
-  });
-
-  final double? width;
-  final double? height;
-  final double radius;
-  final double padding;
-  final Widget? child;
-  final Color backgroundColor;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: height,
-      padding: const EdgeInsets.all(0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(400),
-        color: TColors.textWhite.withOpacity(0.1),
-      ),
-      child: child,
     );
   }
 }
