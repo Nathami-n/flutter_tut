6543cc5343358280522809a7bc22604a97626f86
Nathami-n <georgenathan010@gmail.com>
2025-03-13 00:28:46 +0300

    add: the obs controller and bottom nav obs

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -9,30 +9,32 @@ class NavigationMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = Get.put(NavigationController());
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        height: 80,
-        elevation: 0,
-        selectedIndex: controller.selectedIndex.value,
-        onDestinationSelected: (index) =>
-            {controller.selectedIndex.value = index},
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Iconsax.home),
-            label: "Home",
-          ),
-          NavigationDestination(
-            icon: Icon(Iconsax.shop),
-            label: "Store",
-          ),
-          NavigationDestination(
-            icon: Icon(Iconsax.heart),
-            label: "Wishlist",
-          ),
-          NavigationDestination(
-            icon: Icon(Iconsax.user),
-            label: "Profile",
-          ),
-        ],
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) =>
+              {controller.selectedIndex.value = index},
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Iconsax.home),
+              label: "Home",
+            ),
+            NavigationDestination(
+              icon: Icon(Iconsax.shop),
+              label: "Store",
+            ),
+            NavigationDestination(
+              icon: Icon(Iconsax.heart),
+              label: "Wishlist",
+            ),
+            NavigationDestination(
+              icon: Icon(Iconsax.user),
+              label: "Profile",
+            ),
+          ],
+        ),
       ),
       body: Container(),
     );
