9489615e89145cd2e7b37c519b13d3cace103ae1
Nathami-n <georgenathan010@gmail.com>
2025-03-12 23:52:10 +0300

    feat: refatored the success_screen and reset_password to demarcate the scaffold

--- lib/features/authentication/screens/password_configuration/reset_password.dart
+++ lib/features/authentication/screens/password_configuration/reset_password.dart
@@ -1,7 +1,9 @@
 import 'package:e_commerce/common/widgets/success_screens/success_screen.dart';
+import 'package:e_commerce/features/authentication/screens/login/login.dart';
 import 'package:e_commerce/utils/constants/image_strings.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:e_commerce/utils/constants/text_strings.dart';
+import 'package:e_commerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -26,11 +28,41 @@ class ResetPassword extends StatelessWidget {
           padding: const EdgeInsets.all(TSizes.defaultSpace),
           child: Column(
             children: [
-              SuccessScreen(
-                image: TImages.deliveredEmailIllustration,
-                title: TTexts.changeYourPasswordTitle,
-                subTitle: TTexts.changeYourPasswordSubTitle,
-                onPressed: () => {},
+              Image(
+                image: const AssetImage(
+                  TImages.deliveredEmailIllustration,
+                ),
+                width: THelperFunctions.screenWidth() * 0.6,
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+              // title and subtitle
+              Text(
+                TTexts.changeYourPasswordTitle,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwItems,
+              ),
+              Text(
+                TTexts.changeYourPasswordSubTitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+
+              //buttons
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => {Get.off(() => const LoginScreen())},
+                  child: const Text(TTexts.tContinue),
+                ),
               ),
               const SizedBox(height: TSizes.spaceBtwItems),
               SizedBox(
