1253261ba35dbbd0a352d4eacd740d6cfb418e13
Nathami-n <georgenathan010@gmail.com>
2025-03-13 01:07:17 +0300

    feat: separated the circular field

--- lib/features/authentication/screens/home/home.dart
+++ lib/features/authentication/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,55 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: TColors.primary,
+              padding: const EdgeInsets.all(0),
+              child: Stack(
+                children: [
+                  CircularContainerBackground(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CircularContainerBackground extends StatelessWidget {
+  const CircularContainerBackground({
+    super.key,
+    this.width = 400,
+    this.height = 400,
+    required this.radius,
+    required this.padding,
+    this.child,
+    required this.backgroundColor,
+  });
+
+  final double? width;
+  final double? height;
+  final double radius;
+  final double padding;
+  final Widget? child;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: const EdgeInsets.all(0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(400),
+        color: TColors.textWhite.withOpacity(0.1),
+      ),
+      child: child,
+    );
   }
 }
