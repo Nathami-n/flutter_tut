268f1833f963d4d440f9c967e56eeed1bf2974a2
Nathami-n <georgenathan010@gmail.com>
2025-03-12 13:44:17 +0300

    add: the textSpan

--- lib/features/authentication/screens/signup/signup.dart
+++ lib/features/authentication/screens/signup/signup.dart
@@ -1,5 +1,7 @@
+import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:e_commerce/utils/constants/text_strings.dart';
+import 'package:e_commerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 
@@ -8,6 +10,7 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
     return Scaffold(
         appBar: AppBar(),
         body: SingleChildScrollView(
@@ -89,6 +92,50 @@ class SignUpScreen extends StatelessWidget {
                             labelText: TTexts.password,
                             prefixIcon: Icon(Iconsax.lock),
                           ),
+                        ),
+                        const SizedBox(
+                          height: TSizes.spaceBtwSections,
+                        ),
+
+                        // terms and conditions
+                        Row(
+                          children: [
+                            SizedBox(
+                              height: 24,
+                              width: 24,
+                              child:
+                                  Checkbox(value: true, onChanged: (value) {}),
+                            ),
+                            const SizedBox(
+                              width: TSizes.spaceBtwItems,
+                            ),
+                            Text.rich(
+                              TextSpan(
+                                children: [
+                                  TextSpan(
+                                      text: "${TTexts.iAgreeTo} ",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall),
+                                  TextSpan(
+                                    text: TTexts.privacyPolicy,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyMedium!
+                                        .apply(
+                                            color: dark
+                                                ? TColors.white
+                                                : TColors.primary),
+                                  ),
+                                  TextSpan(
+                                      text: "${TTexts.and} ",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall),
+                                ],
+                              ),
+                            )
+                          ],
                         )
                       ],
                     ),
