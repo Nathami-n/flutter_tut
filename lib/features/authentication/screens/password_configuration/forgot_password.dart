d02bca528cf8d84ef13d13e4d761ddca0b3c3047
Nathami-n <georgenathan010@gmail.com>
2025-03-12 23:26:35 +0300

    feat: added the forgot_password and reset_password screens

--- lib/features/authentication/screens/password_configuration/forgot_password.dart
+++ lib/features/authentication/screens/password_configuration/forgot_password.dart
@@ -1,6 +1,8 @@
+import 'package:e_commerce/features/authentication/screens/password_configuration/reset_password.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:e_commerce/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 class ForgotPassword extends StatelessWidget {
@@ -10,34 +12,49 @@ class ForgotPassword extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(TSizes.defaultSpace),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              TTexts.forgetPasswordTitle,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const SizedBox(
-              height: TSizes.spaceBtwItems,
-            ),
-            Text(
-              TTexts.forgetPasswordSubTitle,
-              style: Theme.of(context).textTheme.labelMedium,
-            ),
-            const SizedBox(
-              height: TSizes.spaceBtwSections,
-            ),
-
-            // input field
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: TTexts.email,
-                prefixIcon: Icon(Iconsax.direct_right),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                TTexts.forgetPasswordTitle,
+                style: Theme.of(context).textTheme.headlineMedium,
               ),
-            )
-          ],
+              const SizedBox(
+                height: TSizes.spaceBtwItems,
+              ),
+              Text(
+                TTexts.forgetPasswordSubTitle,
+                style: Theme.of(context).textTheme.labelMedium,
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+
+              // input field
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: TTexts.email,
+                  prefixIcon: Icon(Iconsax.direct_right),
+                ),
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+              // submit button
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Get.off(() => const ResetPassword());
+                  },
+                  child: const Text(TTexts.submit),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/features/authentication/screens/password_configuration/reset_password.dart
@@ -0,0 +1,49 @@
+import 'package:e_commerce/common/widgets/success_screens/success_screen.dart';
+import 'package:e_commerce/utils/constants/image_strings.dart';
+import 'package:e_commerce/utils/constants/sizes.dart';
+import 'package:e_commerce/utils/constants/text_strings.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ResetPassword extends StatelessWidget {
+  const ResetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () => Get.back(),
+            icon: const Icon(CupertinoIcons.clear),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              SuccessScreen(
+                image: TImages.deliveredEmailIllustration,
+                title: TTexts.changeYourPasswordTitle,
+                subTitle: TTexts.changeYourPasswordSubTitle,
+                onPressed: () => {},
+              ),
+              const SizedBox(height: TSizes.spaceBtwItems),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(TTexts.resendEmail),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
