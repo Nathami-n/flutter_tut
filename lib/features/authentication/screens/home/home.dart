7253b5cac7444c69d14ccf49f33737fa202f6df9
Nathami-n <georgenathan010@gmail.com>
2025-03-13 07:35:38 +0300

    add: the final form or the shape

--- lib/features/authentication/screens/home/home.dart
+++ lib/features/authentication/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/common/widgets/common_shapes/containers/circular_container.dart';
+import 'package:e_commerce/common/widgets/common_shapes/curved_edges/curved_ages.dart';
 import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -11,28 +12,31 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              color: TColors.primary,
-              padding: const EdgeInsets.all(0),
-              child: SizedBox(
-                height: 400,
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: -150,
-                      right: -250,
-                      child: CircularContainerBackground(
-                        backgroundColor: TColors.textWhite.withOpacity(0.1),
+            ClipPath(
+              clipper: TCustomCurvedEdges(), 
+              child: Container(
+                color: TColors.primary,
+                padding: const EdgeInsets.all(0),
+                child: SizedBox(
+                  height: 400,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: -150,
+                        right: -250,
+                        child: CircularContainerBackground(
+                          backgroundColor: TColors.textWhite.withOpacity(0.1),
+                        ),
                       ),
-                    ),
-                    Positioned(
-                      top: 100,
-                      right: -300,
-                      child: CircularContainerBackground(
-                        backgroundColor: TColors.textWhite.withOpacity(0.1),
+                      Positioned(
+                        top: 100,
+                        right: -300,
+                        child: CircularContainerBackground(
+                          backgroundColor: TColors.textWhite.withOpacity(0.1),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
